aa09bff0d7c8a7ad8cded1352bb9a75ec01c3f51
Jeremias ramirez <Jereramirez499@gmail.com>
2021-10-22 23:10:59 -0400

    Creado componente navbar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <div id="app">
-     
+     <NavbarComponent />
   </div>
 </template>
 
 <script>
- 
+ import NavbarComponent from "./components/navbar/NavbarComponent.vue";
+
 export default {
   name: 'App',
   components: {
-    
+    NavbarComponent
   }
 }
 </script>
---
+++ src/components/navbar/NavbarComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <nav class="navbar">
+   
+        <h1 class="title__navbar">Counter</h1>
+    </nav>
+</template>
+
+<script>
+
+
+export default {
+    data() {
+        
+    }
+}
+</script>
+
+
+
+
+<style>
+    .navbar{
+        background-color:rgb(20, 20, 177);
+        padding:15px 0;
+        border-bottom-left-radius:5px;
+        border-bottom-right-radius:5px;
+    }
+    .title__navbar{
+        margin:0;
+        padding:0;
+
+        text-align: center;
+        font-family: "ubuntu";
+        color: white;
+        font-size:17px;
+    }
+   
+</style>
