eb5e9aa23858fcf50a7473dcdf64918f9125e694
Jeremias ramirez <Jereramirez499@gmail.com>
2021-10-29 12:48:03 -0400

    agregado componente de slashcreeen para cargas

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,43 @@
 <template>
   <div id="app">
 
-   
-      <NavbarComponent />
-    <TimeComponent  />
-   
+      <SplashsCreen   v-if="!isCharged"/>
+
+      <section v-if="isCharged">
+        <NavbarComponent />
+        <TimeComponent  />
+      </section>
+
+      {{charged()}}
   </div>
 </template>
 
 <script>
- import NavbarComponent from "./components/navbar/NavbarComponent.vue";
+import SplashsCreen from "./components/splash/SplashScreen.vue";
+import NavbarComponent from "./components/navbar/NavbarComponent.vue";
 import TimeComponent from './components/time/TimeComponent.vue';
 
 export default {
   name: 'App',
   components: {
     NavbarComponent,
-    TimeComponent
+    TimeComponent,
+    SplashsCreen
+  },
+  data() {
+    return {
+      isCharged:false
+    }
+  },
+  methods :{
+      
+      charged(){
+        setTimeout(()=>{
+          this.isCharged=true;
+        },2000)
+          
+      }
+ 
   }
 }
 </script>
---
+++ src/components/splash/SplashScreen.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Splash</h1>
+    </div>
+</template>
+
